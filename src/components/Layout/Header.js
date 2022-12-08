951f2bf3e38cc19b5c649b1006771843363a5647
Farah <f.deshnawy@aucegypt.edu>
2022-12-08 15:46:14 +0200

    chnges

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,10 @@ import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
+
     <Fragment>
+
+      
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
